30e209c1f5bec6e25d51f9f1afe7ccf4d05ea7ec
Sanny Mulyono <smulyono@me.com>
2017-05-07 11:00:52 -0700

    init workspace with react-create-app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import BottomControl from 'components/BottomControl';
+import 'assets/styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BottomControl />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/BottomControl.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class BottomControl extends Component {
+    contructor(){
+
+    }
+    render() {
+        return(
+            <footer>
+                FROM BOTTOM
+            </footer>
+        )
+    }
+}
+
+export default BottomControl;
