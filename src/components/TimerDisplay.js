e39ef8172402a660f2eb4ca6de5b9c37f80c10fd
Sanny Mulyono <smulyono@me.com>
2017-05-24 11:18:18 -0700

    change the layout and style

--- src/components/TimerDisplay.js
+++ src/components/TimerDisplay.js
@@ -2,20 +2,46 @@ import React, { Component } from 'react';
 import 'assets/styles/TimerDisplay.css';
 
 class TimerDisplay extends Component {
+    constructor(props) {
+        super(props);
+        this.adjustdisplay = this.adjustdisplay.bind(this);
+    }
+
     render() {
         const {
             clockDisplay,
             isPassThreshold=false
         } = this.props;
 
+        var {hours,minutes,seconds} = this.adjustdisplay(clockDisplay);
+
         return(
             <div className='timerdisplay-container'>
                 <div className={'timerdisplay-watch '+ (isPassThreshold ? "warning" : "")}>
-                    {clockDisplay}
+                    <div>{hours}</div>
+                    <div className='label'>Hours</div>
+                </div>
+                <div className='timerdisplay-watch'>:</div>
+                <div className={'timerdisplay-watch '+ (isPassThreshold ? "warning" : "")}>
+                    <div>{minutes}</div>
+                    <div className='label'>Minutes</div>
+                </div>
+                <div className='timerdisplay-watch'>:</div>
+                <div className={'timerdisplay-watch '+ (isPassThreshold ? "warning" : "")}>
+                    <div>{seconds}</div>
+                    <div className='label'>Seconds</div>
                 </div>
             </div>
         )
     }
+    adjustdisplay(seconds) {
+        return {
+            "hours" : 0,
+            "minutes" : 0,
+            "seconds" : 0
+        }
+    }
+
 }
 
 export default TimerDisplay;
