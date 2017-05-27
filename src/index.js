d124a7bd31a6dda10f59881e910f5d3338a5f636
Sanny Mulyono <smulyono@me.com>
2017-05-27 12:27:31 -0700

    Upgrade to react-scripts 1.0.6, include the paw setup.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
 import 'assets/styles/index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
+
+registerServiceWorker();
 
 if (module.hot) {
     module.hot.accept();
